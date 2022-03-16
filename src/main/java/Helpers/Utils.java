67dba81232d5b0d6a4bd0ce1e39a08df88fb3dbb
frimpongOpoku <pongofrimi@gmail.com>
2022-03-17 02:10:06 +0400

    Added utils class

---
+++ src/main/java/Helpers/Utils.java
@@ -0,0 +1,12 @@
+
+package Helpers;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * 
+ * @author Frimpong Opoku Agyemang
+ */
+public class Utils {
+     public static final SimpleDateFormat formatter = new SimpleDateFormat("dd MMMM, yyyy HH:mm a");
+}
