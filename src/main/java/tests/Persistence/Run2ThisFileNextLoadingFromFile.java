18cc59fc7d03a5ef08493851300ce175abaa7c10
frimpongOpoku <pongofrimi@gmail.com>
2022-03-19 21:37:26 +0400

    Added test files

---
+++ src/main/java/tests/Persistence/Run2ThisFileNextLoadingFromFile.java
@@ -0,0 +1,18 @@
+package tests.Persistence;
+
+import com.mycompany.dsadoublelinkedlist.classes.StudentList;
+
+/**
+ *
+ * @author Frimpong Opoku Agyemang
+ */
+public class Run2ThisFileNextLoadingFromFile {
+
+    public static void main(String[] args) {
+        StudentList list = new StudentList();
+        list.loadFromFile("Test-save-file.txt");
+        System.out.println(String.format(" %s student records loaded", list.length));
+        System.out.println(String.format("----- Displaying all %s student records loaded file",list.length));
+        list.display();
+    }
+}
